97e130198f706a094473fcd8db1308834c9ab46f
PatriQ94 <patro11@hotmail.com>
2022-08-31 13:16:03 +0200

    Initial commit

---
+++ NET6APITemplate/NET6APITemplate.API/Program.cs
@@ -0,0 +1,36 @@
+var builder = WebApplication.CreateBuilder(args);
+
+ConfigureServices(builder.Services);
+
+var app = builder.Build();
+
+Configure(app);
+
+
+void ConfigureServices(IServiceCollection services)
+{
+    // Add services to the container.
+    services.AddControllers();
+
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen();
+}
+
+void Configure(WebApplication app)
+{
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+}
